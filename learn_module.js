28d80a6a15e1ea9f21e68d946469ee90e770b557
Salman Shaikh <coder88salman@gmail.com>
2024-05-08 09:36:46 +0530

    name

--- learn_module.js
+++ learn_module.js
@@ -1,1 +1,12 @@
 
+var http = require('http');
+
+
+
+
+http.createServer(function (req, res) {
+    const name = req.url.split("=")
+    console.log(name[1])
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`<h1>${name[1]}</h1>`);
+}).listen(8080);
